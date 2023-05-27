fb40e9308488b5f2057cadff3f242e8760856d5d
민주 정 <minju9711@naver.com>
2023-05-27 16:23:29 +0900

    todo_list 새로빼서 진행할 것임 여기 중단!

--- planner.js
+++ planner.js
@@ -3,9 +3,11 @@ function init() {
   const tbody = document.getElementById('tbody');
 
   let myArray = Array.from({ length: 7 }, (_, index) => index + 1);
+  let row = 1;
+  let column = 1;
 
   myArray.forEach(function (item) {
-    
+
     const tr = document.createElement('tr');
     const td_1 = document.createElement('td');
     const td_2 = document.createElement('td');
@@ -14,9 +16,16 @@ function init() {
 
     input_1.type = 'text';
     td_1.textContent = ""; 
+    td_1.setAttribute('id',`{row}_{column`);
+
+    column++;
 
     input_2.type = 'text'; 
-    td_2.textContent = ""; 
+    td_2.textContent = "";
+    td_2.setAttribute('id',`{row}_{column`); 
+
+    row++;
+    columm=1;
 
     td_1.appendChild(input_1);
     td_2.appendChild(input_2);
@@ -24,8 +33,15 @@ function init() {
     tr.appendChild(td_1);
     tr.appendChild(td_2);
     tbody.appendChild(tr);
+
+    if(row===7) plus_btn();
   });
 
 }
 
 init();
+
+function plus_btn(){
+  
+}
+
