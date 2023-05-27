71488f214a13148b3d04049aa5b74d2bf47c3c68
민주 정 <minju9711@naver.com>
2023-05-27 14:58:40 +0900

    todo_list의 추가 버튼 시작

--- planner.js
+++ planner.js
@@ -1,15 +1,31 @@
-function init(){
-  const thead = document.getElementById('thead');
+function init() {
+  const table = document.getElementById('table');
   const tbody = document.getElementById('tbody');
 
-  const th1 = document.createElement('th');
-  th1.innerHTML = '과목';
-  const th2 = document.createElement('th');
-  th2.innerHTML = '할 일';
-  thead.appendChild(th1);
-  thead.appendChild(th2);
+  let myArray = Array.from({ length: 7 }, (_, index) => index + 1);
 
-  forEach(Array.from(length:7));
+  myArray.forEach(function (item) {
+    
+    const tr = document.createElement('tr');
+    const td_1 = document.createElement('td');
+    const td_2 = document.createElement('td');
+    const input_1 = document.createElement('input');
+    const input_2 = document.createElement('input');
+
+    input_1.type = 'text';
+    td_1.textContent = ""; 
+
+    input_2.type = 'text'; 
+    td_2.textContent = ""; 
+
+    td_1.appendChild(input_1);
+    td_2.appendChild(input_2);
+
+    tr.appendChild(td_1);
+    tr.appendChild(td_2);
+    tbody.appendChild(tr);
+  });
+
 }
 
 init();
