29ded77cc5b1891255d6fa9111d22dc331e6ada1
민주 정 <minju9711@naver.com>
2023-06-05 20:02:22 +0900

    todo_list 붙인 결과

--- planner.js
+++ planner.js
@@ -1,47 +1,84 @@
+let number = 1;
+
 function init() {
-  const table = document.getElementById('table');
-  const tbody = document.getElementById('tbody');
+    let myArray = Array.from({ length: 7 }, (_, index) => index + 1);
+    myArray.forEach(function (item) {
+        push_plus();
+    });
+}
 
-  let myArray = Array.from({ length: 7 }, (_, index) => index + 1);
-  let row = 1;
-  let column = 1;
+window.addEventListener('load', init());
+//무조건 이 함수 먼저 실행되게 해주는 것 -> 제일 최상위객체 window
 
-  myArray.forEach(function (item) {
+function push_plus() {
+    if (number !== 1) {
+        const fade_delete = document.getElementById(`delete_btn${number - 1}`);
+        const fade_plus = document.getElementById(`plus_btn${number - 1}`);
+        fade_delete.style.display = 'none';
+        fade_plus.style.display = 'none';
+    }
+
+    const tbody = document.getElementById('tbody');
 
     const tr = document.createElement('tr');
-    const td_1 = document.createElement('td');
-    const td_2 = document.createElement('td');
+    const btn_td = document.createElement('td');
+
     const input_1 = document.createElement('input');
     const input_2 = document.createElement('input');
 
+    const check_btn = document.createElement('button');
+    const plus_btn = document.createElement('button');
+    const delete_btn = document.createElement('button');
+
+    delete_btn.setAttribute('id', `delete_btn${number}`);
+    plus_btn.setAttribute('id', `plus_btn${number}`);
+
     input_1.type = 'text';
-    td_1.textContent = ""; 
-    td_1.setAttribute('id',`{row}_{column`);
+    tr.textContent = "";
+    tr.setAttribute('id', `${number}`);
 
-    column++;
+    input_2.type = 'text';
+    input_2.textContent = "";
 
-    input_2.type = 'text'; 
-    td_2.textContent = "";
-    td_2.setAttribute('id',`{row}_{column`); 
+    number++;
+    plus_btn.textContent = " + ";
+    check_btn.textContent = "no";
+    delete_btn.textContent = " - ";
 
-    row++;
-    columm=1;
+    btn_td.appendChild(delete_btn); 
+    btn_td.appendChild(plus_btn);
 
-    td_1.appendChild(input_1);
-    td_2.appendChild(input_2);
+    tr.appendChild(btn_td); 
+    tr.appendChild(input_1);
+    tr.appendChild(input_2);
+    tr.appendChild(check_btn);
 
-    tr.appendChild(td_1);
-    tr.appendChild(td_2);
     tbody.appendChild(tr);
+    // tbody.insertBefore(tr, tbody.children[number - 1]);
 
-    if(row===7) plus_btn();
-  });
+    plus_btn.addEventListener('click', push_plus); // 노션 문제점 1 참고
+    delete_btn.addEventListener('click', delete_row);
+    check_btn.addEventListener('click', function (event) { // 노션 문제점 2 참고
+        push_check(event);
+    });
+}
+
+function push_check(event) {
+    const check_btn = event.target;
+    const check_text = check_btn.textContent;
+    check_btn.textContent = check_text === "no" ? "yes" : "no";
+}
+
+function delete_row() {
+    const delete_target = document.getElementById(`${number - 1}`); // tr 태그라는걸 알게 해줘야 된다.
+    delete_target.remove();
+    number--;
+
+    const show_delete = document.getElementById(`delete_btn${number - 1}`);
+    const show_plus = document.getElementById(`plus_btn${number - 1}`);
+    show_delete.style.display = 'inline';
+    show_plus.style.display = 'inline';
 
 }
 
-init();
-
-function plus_btn(){
-  
-}
-
+//콜백함수 써보기!! 
